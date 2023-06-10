eff490d6c55db034e7abd1d8038423e399224f2b
indoorism <anneok23@gmail.com>
2023-06-09 23:21:39 -0700

    go

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+
+}
+
+/*===== MENU HIDDEN ========*/
+
+
+/*===== REMOVE MENU MOBILE =====*/
+
+/*===== ACCORDION SKILLS =====*/
+
+/*===== QUALIFICATION TABS =====*/
